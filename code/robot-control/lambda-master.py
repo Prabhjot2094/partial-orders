9a32f6b282257a6b7b277edd5b3cd05acbc19962
pushpreet <hanspal.pushpreet@gmail.com>
2017-04-12 12:59:24 +0000

    add new control script

---
+++ code/robot-control/lambda-master.py
@@ -0,0 +1,24 @@
+import smbus
+import time
+
+bus = smbus.SMBus(1)
+
+address = 0x04
+
+def writeNumber(cmd, value):
+    bus.write_word_data(address, cmd, value)
+    # bus.write_byte_data(address, 0, value)
+    return -1
+
+def readNumber():
+    number = bus.read_byte(address)
+    # number = bus.read_byte_data(address, 1)
+    return number
+
+while True:
+    var1 = input("Enter Left Speed: ")
+    var2 = input("Enter Right Speed: ")
+
+    writeNumber(0, var1)
+    writeNumber(1, var2)
+    # sleep one second
