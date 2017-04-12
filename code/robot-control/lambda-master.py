0521e9d23167d2e47b78d6e818fae5d13bf1a325
pushpreet <hanspal.pushpreet@gmail.com>
2017-04-12 19:23:22 +0530

    change data transfer format

--- code/robot-control/lambda-master.py
+++ code/robot-control/lambda-master.py
@@ -5,20 +5,19 @@ bus = smbus.SMBus(1)
 
 address = 0x04
 
-def writeNumber(cmd, value):
-    bus.write_block_data(address, cmd, value)
-    # bus.write_byte_data(address, 0, value)
-    return -1
+def highByte (number) : return number >> 8
+def lowByte (number) : return number & 0x00FF
+
+def sendMotorSpeeds(speedLeft, speedRight):
+    bus.write_block_data(address, 0, [lowByte(speedLeft), highByte(speedLeft), lowByte(speedRight), highByte(speedRight)])
 
 def readNumber():
     number = bus.read_byte(address)
-    # number = bus.read_byte_data(address, 1)
+
     return number
 
 while True:
     var1 = input("Enter Left Speed: ")
     var2 = input("Enter Right Speed: ")
 
-    writeNumber(0, [var1, var2])
-    #writeNumber(1, var2)
-    # sleep one second
+    sendMotorSpeeds(var1, var2)
