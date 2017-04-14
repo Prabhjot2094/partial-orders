a4162d5cb64c31fb7e3b567b6e2ab24b78d374c4
pushpreet <hanspal.pushpreet@gmail.com>
2017-04-14 17:19:24 +0530

    map i2c data

--- code/robot-control/lambda-master.py
+++ code/robot-control/lambda-master.py
@@ -2,24 +2,26 @@ import smbus
 import time
 
 bus = smbus.SMBus(1)
-
 address = 0x04
+
+sensorData = []
 
 def highByte (number) : return number >> 8
 def lowByte (number) : return number & 0x00FF
 def getWord (lowByte, highByte) : return ((highByte << 8) | lowByte)
 
-def sendMotorSpeeds(speedLeft, speedRight):
+def writeMotorSpeeds(speedLeft, speedRight):
     bus.write_block_data(address, 0, [highByte(speedLeft), lowByte(speedLeft), highByte(speedRight), lowByte(speedRight)])
 
-def readNumber():
-    data = bus.read_i2c_block_data(address, 0)
-    print number
+def readSensorData():
+    rawData = bus.read_i2c_block_data(address, 0)
+    for i in range(0, 11):
+        print getWord(rawData[2*i + 1], rawData[2*i +0])
 
 while True:
+    readSensorData()
+    
     var1 = input("Enter Left Speed: ")
     var2 = input("Enter Right Speed: ")
 
-    sendMotorSpeeds(var1, var2)
-
-    readNumber()
+    writeMotorSpeeds(var1, var2)
