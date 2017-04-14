709172366ec5cb7747a13a8fba733e56d5ef3980
pushpreet <hanspal.pushpreet@gmail.com>
2017-04-14 17:49:34 +0530

    finalise i2c data transfer

--- code/robot-control/lambda-master.py
+++ code/robot-control/lambda-master.py
@@ -3,8 +3,9 @@ import time
 
 bus = smbus.SMBus(1)
 address = 0x04
+sensorCount = 11
 
-sensorData = []
+sensorData = [0] * sensorCount
 
 def highByte (number) : return number >> 8
 def lowByte (number) : return number & 0x00FF
@@ -15,11 +16,13 @@ def writeMotorSpeeds(speedLeft, speedRight):
 
 def readSensorData():
     rawData = bus.read_i2c_block_data(address, 0)
-    for i in range(0, 11):
-        print getWord(rawData[2*i + 1], rawData[2*i +0])
+    for sensorIndex in range(0, sensorCount):
+        sensorData[sensorIndex] = getWord(rawData[2*i + 1], rawData[2*i +0])
 
 while True:
     readSensorData()
+
+    print sensorData
     
     var1 = input("Enter Left Speed: ")
     var2 = input("Enter Right Speed: ")
