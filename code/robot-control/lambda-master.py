35bc4e5a753e25be5c4dc1b308635f46732e5fc3
pushpreet <hanspal.pushpreet@gmail.com>
2017-04-14 16:42:02 +0530

    complete i2c transfer

--- code/robot-control/lambda-master.py
+++ code/robot-control/lambda-master.py
@@ -7,17 +7,19 @@ address = 0x04
 
 def highByte (number) : return number >> 8
 def lowByte (number) : return number & 0x00FF
+def getWord (lowByte, highByte) : return ((highByte << 8) | lowByte)
 
 def sendMotorSpeeds(speedLeft, speedRight):
     bus.write_block_data(address, 0, [highByte(speedLeft), lowByte(speedLeft), highByte(speedRight), lowByte(speedRight)])
 
 def readNumber():
-    number = bus.read_byte(address)
-
-    return number
+    data = bus.read_i2c_block_data(address, 0)
+    print number
 
 while True:
     var1 = input("Enter Left Speed: ")
     var2 = input("Enter Right Speed: ")
 
     sendMotorSpeeds(var1, var2)
+
+    readNumber()
