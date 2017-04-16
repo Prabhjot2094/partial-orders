714192e84ec8e2d7e15b1ed1015ca872eee92849
pushpreet <hanspal.pushpreet@gmail.com>
2017-04-16 14:06:16 +0530

    integrate tile and arduino data

--- code/robot-control/lambda-master.py
+++ code/robot-control/lambda-master.py
@@ -13,14 +13,14 @@ AUTOPILOT_COMPUTE_INTERVAL  = 50    # milliseconds
 arduinoBus = smbus.SMBus(1)
 sensorTile = serial.Serial('/dev/ttyACM0', 9600)
 
-sensorData = [0] * (1 + SENSOR_COUNT + SENSOR_TILE_DATA_COUNT)
+sensorData = [0] * (1 + ARDUINO_DATA_COUNT + SENSOR_TILE_DATA_COUNT)
 sensorDataReady = False
 dataReadFlag = False
-averageInterval = 0
 
 def highByte (number) : return number >> 8
 def lowByte (number) : return number & 0x00FF
 def getWord (lowByte, highByte) : return ((highByte << 8) | lowByte)
+
 
 def main():
     global dataReadFlag
@@ -31,6 +31,7 @@ def main():
         dataReadThread.start()
     except Exception as e:
         print "Exception in dataReadThread " + e
+        shutdown()
 
     dataReadFlag = False
 
@@ -38,13 +39,52 @@ def main():
         sensorTile.flushInput()
         print sensorTile.readline()
         time.sleep(0.500)
-        
+
+
+def arduinoDataHandler():
+    global sensorData
+
+    try:
+        rawData = arduinoBus.read_i2c_block_data(ARDUINO_ADDRESS, 0)
+
+        if (len(rawData) != 32):
+            readSensorData()
+
+        for sensorIndex in range(1, ARDUINO_DATA_COUNT + 1):
+            sensorData[sensorIndex] = getWord(rawData[2*(sensorIndex-1) + 1], rawData[2*(sensorIndex-1) + 0])
+            if sensorData[sensorIndex] > 1023:
+                arduinoDataHandler()
+
+    except IOError:
+        arduinoDataHandler()
+
+
+def sensorTileDataHandler():
+    global sensorData
+
+    try:
+        sensorTile.flushInput()
+
+        while True:
+            rawData = sensorTile.readline()
+            if len(rawData) == 24:
+                for sensorIndex in range(0, SENSOR_TILE_DATA_COUNT):
+                    sensorData[(1 + ARDUINO_DATA_COUNT) + sensorIndex] = rawData[sensorIndex]
+
+            else:
+                continue
+
+    except Exception as e:
+        print "Exception in sensorTileDataHandler " + e
+        shutdown()
+
 
 def writeMotorSpeeds(speedLeft, speedRight):
     try:
         arduinoBus.write_block_data(ARDUINO_ADDRESS, 0, [highByte(speedLeft), lowByte(speedLeft), highByte(speedRight), lowByte(speedRight)])
     except IOError:
         writeMotorSpeeds(speedLeft, speedRight)
+
 
 def readSensorData():
     global sensorData
@@ -55,31 +95,21 @@ def readSensorData():
 
     while True:
         if dataReadFlag:
-            #sensorDataReady = False
             currentTime = time.time()
             if currentTime >= nextDataReadTime:
+                sensorDataReady = False
+
                 nextDataReadTime += DATA_READ_INTERVAL/100.0
-                print '{0:.8f}'.format(currentTime)
+                
+                sensorData[0] = currentTime
+                arduinoDataHandler()
+                sensorTileDataHandler()
 
-            #sensorDataReady = True
-        
-    '''
-    try:
-        sensorData[0] = time.time()
-        rawData = arduinoBus.read_i2c_block_data(ARDUINO_ADDRESS, 0)
+                sensorDataReady = True
 
-        if (len(rawData) != 32):
-            readSensorData()
+        else:
+            time.sleep(0.01)
 
-
-        for sensorIndex in range(1, SENSOR_COUNT + 1):
-            sensorData[sensorIndex] = getWord(rawData[2*(sensorIndex-1) + 1], rawData[2*(sensorIndex-1) + 0])
-            if sensorData[sensorIndex] > 1023:
-                readSensorData()
-
-    except IOError:
-        readSensorData()
-    '''
 
 def drive(command, speed=127):
     global dataReadFlag
@@ -110,5 +140,10 @@ def drive(command, speed=127):
 
     if command == 'autopilot-sonar-yaw':
         pass
+
+
+def shutdown():
+    writeMotorSpeeds(0, 0)
+    sys.exit(0)
 
 main()
