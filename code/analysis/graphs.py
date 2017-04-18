f254d5d22c691d8c94db6c1daa07ca03f52bf824
Prabhjot2094 <prabhjotsingh467@gmail.com>
2017-04-18 14:55:36 +0530

    client ip stored in _HOST

--- code/analysis/graphs.py
+++ code/analysis/graphs.py
@@ -275,9 +275,10 @@ if __name__ == '__main__':
     processedData = Queue()
     try:
         try:
-                graphToPlot = sys.argv[1]
-                dataRequestParams = sys.argv[2:]
+                graphToPlot = sys.argv[1]		#line, scatter,scatter_3d,line_3d aas first parameter
+                dataRequestParams = sys.argv[2:]#(c), (s), (f,frequency)
         except:
+        		#Default graph and Request parameters
                 graphToPlot = "scatter_3d"
                 dataRequestParams = ['c']
 
